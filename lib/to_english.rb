812ea8ebc33c71364a6c99de458171198efbfab9
loganjacob76 <nagolisback@gmail.com>
2021-03-08 23:33:30 -0700

    Create and test ability to sort input into proper format

--- lib/to_english.rb
+++ lib/to_english.rb
@@ -8,4 +8,19 @@ class ToEnglish
     @dictionary = Dictionary.new
     @complete_message = ""
   end
+
+  def make_rows
+    raw_text = @message
+    rows = {
+      top: [].join,
+      middle: [].join,
+      bottom: [].join
+    }
+    until raw_text.empty?
+      rows[:top] << raw_text.shift
+      rows[:middle] << raw_text.shift
+      rows[:bottom] << raw_text.shift
+    end
+    rows
+  end
 end
